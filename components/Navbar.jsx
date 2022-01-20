9e63756c7858d051a09c13c04f2db5a33fc8c832
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-20 11:41:21 +0500

    navbar completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,30 +1,30 @@
-import { Box, Flex, Menu ,  MenuButton, MenuList, MenuItem, Spacer, IconButton } from '@chakra-ui/react'
+import { Box, Flex, Menu, MenuButton, MenuList, MenuItem, Spacer, IconButton } from '@chakra-ui/react'
 import Link from 'next/link'
-import {FcMenu , FcAbout , FcKey} from 'react-icons/fc'
+import { FcMenu, FcAbout, FcKey } from 'react-icons/fc'
 import { BsSearch } from 'react-icons/bs'
 import { AiFillHome } from 'react-icons/ai'
 const Navbar = () => {
     return (
         <Flex p='4' borderBottom='1px' borderColor='gray.100'>
             <Box fontSize='3xl' color='blue.400' fontWeight='bold'>
-                <Link href='/' paddingLeft='2'>Realtor</Link>
+                <Link href='/' paddingLeft='3'>Realtor</Link>
             </Box>
             <Spacer />
             <Box>
                 <Menu>
-                    <MenuButton as={IconButton} icon={<FcMenu />} variant='outline' color='red.400' />
+                    <MenuButton as={IconButton} icon={<FcMenu />} variant='outline' />
                     <MenuList>
                         <Link href='/' passHref>
-                            <MenuItem icon={<AiFillHome/>} >Home</MenuItem>
+                            <MenuItem icon={<AiFillHome />} >Home</MenuItem>
                         </Link>
                         <Link href='/search' passHref>
-                            <MenuItem icon={<BsSearch/>} >Search</MenuItem>
+                            <MenuItem icon={<BsSearch />} >Search</MenuItem>
                         </Link>
                         <Link href='/search?purpose=for-sale' passHref>
-                            <MenuItem icon={<FcAbout/>Buy Property</MenuItem>
+                            <MenuItem icon={<FcAbout />}> Buy Property</MenuItem>
                         </Link>
                         <Link href='/search?purpose=for-rent' passHref>
-                            <MenuItem icon={<FcKey/>}>Rent Property</MenuItem>
+                            <MenuItem icon={<FcKey />}>Rent Property</MenuItem>
                         </Link>
                     </MenuList>
                 </Menu>
---
+++ components/Slider.jsx
@@ -0,0 +1,9 @@
+import { Box , Flex } from '@chakra-ui/react'
+import { BsArrowLeft , BsArrowRight } from 'react-icons/bs'
+export default function Slider() {
+    return (
+        <>
+            
+        </>
+    )
+}
