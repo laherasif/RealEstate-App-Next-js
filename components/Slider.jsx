24794812b9bc8f9a432e4d02745f279ec36daa77
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-20 11:44:45 +0500

    create slider

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,9 +1,20 @@
-import { Box , Flex } from '@chakra-ui/react'
-import { BsArrowLeft , BsArrowRight } from 'react-icons/bs'
+import { Box, Flex } from '@chakra-ui/react'
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs'
 export default function Slider() {
     return (
         <>
-            
+            <Box w="100%" p="3" border="1pt solid">
+
+                <Flex>
+                    <BsArrowLeft />
+
+                    <Box>
+
+                    </Box>
+                    <BsArrowRight/>
+
+                </Flex>
+            </Box>
         </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 
 import Navbar from '../components/Navbar';
-
+import Slider from '../components/Slider'
 export default function Home() {
   return (
     <>
       <Navbar/>
+      <Slider/>
     </>
 
   )
