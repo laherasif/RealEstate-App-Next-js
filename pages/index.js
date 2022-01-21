519d5b8f2b5029c6f0e0ba8d3211524af4864ca2
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-21 11:04:49 +0500

    done main content

--- pages/index.js
+++ pages/index.js
@@ -6,27 +6,39 @@ import { Flex, Box, Text, Button } from '@chakra-ui/react'
 import Slider1 from '../assests/images/slider-1.jpg'
 const MianContent = () => {
   return (
-    <Flex flexWrap="wrap" justifyContent="center" textAlign="center" p="2">
-      <Image src={Slider1} width={100} height={50} alt="main_image" />
-      <Flex flexDirection="column" p="5">
-        <Text color="gray.400" fontWeight="medium" fontSize="sm">Explore more </Text>
-        <Text fontSize="3xl" fontWeight="bold">Explore more <br/> titlte 2 </Text>
-        <Text paddingTop="2" paddingBottom="2" fontSize="lg" color="gray.700" >Explore more </Text>
-        <Button>more</Button>
+    <>
+      <Flex flexWrap="wrap" justifyContent="center" textAlign="center" p="2">
+        <Image src={Slider1} width={500} height={300} alt="main_image" />
+        <Flex flexDirection="column" p="5">
+          <Text color="gray.400" fontWeight="medium" fontSize="sm">Explore more </Text>
+          <Text fontSize="3xl" fontWeight="bold">Explore more <br /> titlte 2 </Text>
+          <Text paddingTop="2" paddingBottom="2" fontSize="lg" color="gray.700" >Explore more <br /> desc2 </Text>
+          <Button color="white" fontSize="xlg" bg="blue.200">more</Button>
+        </Flex>
       </Flex>
-    </Flex>
+
+
+
+    </>
+
   )
 }
 
 
 export default function Home() {
   return (
-    <>
+    <Box>
       <Navbar />
       <MianContent />
+      <Flex>
+        property
+      </Flex>
+      <MianContent />
+      <Flex>
+        property
+      </Flex>
 
-
-    </>
+    </Box>
 
   )
 }
