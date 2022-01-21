e969a9b33b1ada6f3f6e5e0269d3643e3a77d579
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-21 23:37:55 +0500

    complete seach by location

--- components/SearchFilterData.jsx
+++ components/SearchFilterData.jsx
@@ -2,8 +2,9 @@
 import { useState, useEffect } from 'react'
 import { Flex, Box, Button, Icon, Spinner, Select, Input, Text } from '@chakra-ui/react'
 import { filterData, getFilterValues } from '../ulit/filterDataOption'
+import { fetchApi, baseUrl } from '../ulit/api'
 import { useRouter } from 'next/router';
-import { MdCancel} from 'react-icons/md'
+import { MdCancel } from 'react-icons/md'
 
 
 export default function SearchFilterData() {
@@ -28,6 +29,23 @@ export default function SearchFilterData() {
 
         router.push({ pathname: path, query })
     }
+
+    useEffect(() => {
+        if (search !== '') {
+            const fetchData = async () => {
+                setLoader(true);
+                const data = await fetchApi(`${baseUrl}/auto-complete?query=${search}`);
+                setLoader(false);
+                setLocation(data ?.hits);
+            };
+
+            fetchData();
+        }
+    }, [search]);
+
+
+
+
     return (
         <Flex bg='gray.100' p='4' flexWrap="wrap" justifyContent="center"  >
             {Filters.map((filter) => {
@@ -49,51 +67,79 @@ export default function SearchFilterData() {
                     </Box>
                 )
             })}
-            
+
             <Flex flexDir="column" marginTop="2" >
                 <Button
-                onClick={()=> setShowLocation(!showlocation)}
-                 borderColor="gray.400"
-                  border="1pt solid">Search By Location </Button>
-
+                    onClick={() => setShowLocation(!showlocation)}
+                    borderColor="gray.400"
+                    border="1pt solid">Search By Location </Button>
                 <Flex flexDir="column" pos="relative" paddingTop="2" >
-                    <Input
-                        w="300px"
-                        focusBorderColor="gray.400"
-                        placeholder="seach by location "
-                    />
-                    <Icon
-                     as={MdCancel}
-                     pos="absolute"
-                     w='25px'
-                     top="5"
-                     right="5"
-                     zIndex="2"
-                     cursor="pointer"
-                    >
+                    {showlocation && (
+                        <Box>
+                            <Input
+                                w="300px"
+                                focusBorderColor="gray.400"
+                                placeholder="seach by location "
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                            />
+                            {location !== '' &&
+                                <Icon
+                                    as={MdCancel}
+                                    pos="absolute"
+                                    w='25px'
+                                    top="5"
+                                    right="5"
+                                    zIndex="2"
+                                    cursor="pointer"
+                                    onClick={() => setSearch('')}
+                                >
 
-                    </Icon>
-                    <Spinner margin="auto" marginTop="2" color="gray.500"/>
+                                </Icon>
+                            }
 
-                    <Box height="300px" overFlow="auto">
-                      <Box>
-                          <Text cursor="pointer" borderBottom="1pt solid" borderColor="gray.100" >location name here</Text>
-                      </Box>
-                       
-                       <Flex justifyContent="center" flexDir="column" alignItems="center">
-                          <Text>Waiting for search </Text>
-                       </Flex>
+                        </Box>
+                    )
+                    }
+                    {loader && <Spinner margin="auto" marginTop="2" color="gray.500" />}
+                    {showlocation &&
+                        <Box height="300px" overFlow="auto">
+                            {location ?.map((location) => (
+                                <Box
+                                    key={location.id}
+                                    onClick={() => {
+                                        searchFilterValue({ locationExternalIDs: location.externalID });
+                                        setShowLocation(false);
+                                        setSearch(location.name);
+                                    }}
+                                >
+                                    <Text cursor='pointer' bg='gray.200' p='2' borderBottom='1px' borderColor='gray.100' >
+                                        {location.name}
+                                    </Text>
+                                </Box>
+                            ))}
+                        </Box>
+                    }
+                    <Box>
+                    {!loader && !location ?.length && (
+                        <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5' >
+                            {/* <Image src={noresult} /> */}
+                            <Text fontSize='xl' marginTop='3'>
+                                Waiting to search!
+                                   </Text>
+                        </Flex>
+                    )}
 
-                    </Box>
-                </Flex>
+                        </Box>
             </Flex>
-
-
-
-
-
-
-
         </Flex>
-    )
+
+
+
+
+
+
+
+            </Flex >
+            )
 }
