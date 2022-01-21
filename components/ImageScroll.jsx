8dcf39029b4da42f3ac184174f7134cb1790577c
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-21 16:35:19 +0500

    create image scroll component

---
+++ components/ImageScroll.jsx
@@ -0,0 +1,16 @@
+import { Flex, Text, Box } from '@chakra-ui/react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { GoVerified } from 'react-icons/go'
+import { FaBed, FaBath } from 'react-icons/fa'
+import { Avatar } from '@chakra-ui/avatar'
+import { BsGridFill } from 'react-icons/bs'
+import {ScrollMenu , VisibilityContext } from 'react-horizontal-scrolling-menu'
+import { useContext } from 'react'
+export default function ImageScroll() {
+    return (
+        <Flex>
+            
+        </Flex>
+    )
+}
