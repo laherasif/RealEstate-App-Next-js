9eb49e7adc066b99306f19ec47ac0e2f673c06ed
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-21 19:05:04 +0500

    add image

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,9 +1,10 @@
 import { Box , Flex , Text } from '@chakra-ui/react'
-
+import slider3 from '../../assests/images/slider-3.jpg'
 export default function SingleProduct() {
     return (
         <Box maxWidth="1000px" margin="auto" p="4">
-          hi this si   
+          <Image src={slider3} width={1000} height={500} alt="prduct_image" />   
+
         </Box>
     )
 }
