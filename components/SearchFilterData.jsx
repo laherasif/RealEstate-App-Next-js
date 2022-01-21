b9172308e4684cad5c10e2c2ca93961f8a11a6bf
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-21 12:30:05 +0500

    creating search filter component

---
+++ components/SearchFilterData.jsx
@@ -0,0 +1,9 @@
+import { useState , useEffect} from 'react'
+import { Flex , Box , Button , Icon , Spinner , Select , Input , Text  } from '@chakra-ui/react'
+export default function SearchFilterData() {
+    return (
+        <div>
+            hellow
+        </div>
+    )
+}
--- pages/search.jsx
+++ pages/search.jsx
@@ -1,11 +1,28 @@
-import { Box , Flex , Text  } from '@chakra-ui/react'
+import { Box, Flex, Text, Icon } from '@chakra-ui/react'
+import { useState } from 'react'
+import { BsFilter } from 'react-icons/bs'
+import { useRouter } from 'next/router';
+
+import SearchFilterData from '../components/SearchFilterData'
 export default function search() {
+    const [searchFilter, setSearchFilter] = useState(false)
+    const router = useRouter()
     return (
         <Box>
-            <Flex>
+            <Flex justifyContent="center"
+                alignItems="center"
+                fontWeight="black"
+                p="2"
+                fontSize="xlg"
+                bg="gray.200"
+                onClick={(prv) => setSearchFilter(!prv)}
+            >
                 <Text>Search Property By Filter </Text>
+                <Icon w="75" marginLeft="2" as={BsFilter} />
             </Flex>
-            
+
+            {searchFilter && <SearchFilterData />}
+
         </Box>
     )
 }
