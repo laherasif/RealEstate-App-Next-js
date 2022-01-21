803f7d051c9b9fd6d32ff00fbf2f59a8f996bed2
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-21 16:46:33 +0500

    image scroll compo updte

--- components/ImageScroll.jsx
+++ components/ImageScroll.jsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Box } from '@chakra-ui/react'
+import { Flex, Text, Box , Icon } from '@chakra-ui/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { GoVerified } from 'react-icons/go'
@@ -7,10 +7,43 @@ import { Avatar } from '@chakra-ui/avatar'
 import { BsGridFill } from 'react-icons/bs'
 import {ScrollMenu , VisibilityContext } from 'react-horizontal-scrolling-menu'
 import { useContext } from 'react'
+import { FaArrowAltCircleLeft , FaArrowCircleRight } from 'react-icons/fa'
 export default function ImageScroll() {
+
+
+      const LeftArrow = () =>{
+          const { scrollPrv } = useContext(VisibilityContext)
+
+          return (
+              <Flex justifyContent="center" alignItems="center" marginLeft="1">
+                  <Icon
+                  as={FaArrowAltCircleLeft}
+                  fontSize="2xl"
+                  cursor="pointer"
+                  onClick={scrollPrv}
+                  />
+              </Flex>
+          )
+      }
+
+      const LeftRight = () =>{
+        const { scrollNext } = useContext(VisibilityContext)
+
+        return (
+            <Flex justifyContent="center" alignItems="center" marginLeft="1">
+                <Icon
+                as={FaArrowAltCircleRight}
+                fontSize="2xl"
+                cursor="pointer"
+                onClick={scrollNext}
+                />
+            </Flex>
+        )
+    }
+
     return (
-        <Flex>
-            
-        </Flex>
+            <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+
+            </ScrollMenu>
     )
 }
