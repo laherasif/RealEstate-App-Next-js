1067be7db937eb1858238b59c8d0d3e5154b20d1
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-22 13:54:56 +0500

    chnage main comp

--- pages/index.js
+++ pages/index.js
@@ -10,13 +10,7 @@ const MianContent = () => {
   return (
     <>
       <Flex flexWrap="wrap" justifyContent="center" textAlign="center" p="2">
-        <Image src={Slider1} width={500} height={300} alt="main_image" />
-        <Flex flexDirection="column" p="5">
           <Text color="gray.400" fontWeight="medium" fontSize="sm">Explore more </Text>
-          <Text fontSize="3xl" fontWeight="bold">Explore more <br /> titlte 2 </Text>
-          <Text paddingTop="2" paddingBottom="2" fontSize="lg" color="gray.700" >Explore more <br /> desc2 </Text>
-          <Button color="white" fontSize="xlg" bg="blue.200">more</Button>
-        </Flex>
       </Flex>
 
 
