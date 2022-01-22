faaf83b18a684100b780fefe3ddb4921a08af8dd
laherasif <47274948+laherasif@users.noreply.github.com>
2022-01-22 14:30:20 +0500

    completed slider

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,9 +1,7 @@
-import { Box, Flex } from '@chakra-ui/react'
-import { BsArrowLeft, BsArrowRight } from 'react-icons/bs'
+import { Box } from '@chakra-ui/react'
 import Image from 'next/image'
 import slider1 from '../assests/images/slider-1.jpg'
 import slider2 from '../assests/images/slider-2.jpg'
-import slider3 from '../assests/images/slider-3.jpg'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 export default function Slider() {
@@ -12,13 +10,13 @@ export default function Slider() {
             <Box pos="relative" p="0">
                 <Carousel>
                     <Box>
-                        <Image width="0%" height="50%" layout="fill" objectFit="cover" src={slider1} />
+                        <Image width="100%" height="40%" layout="responsive" objectFit="cover" src={slider1} />
                     </Box>
                     <Box>
-                        <Image width="0%" height="80%" layout="fill" objectFit="cover" src={slider2} />
+                        <Image width="100%" height="40%" layout="responsive" objectFit="cover" src={slider2} />
                     </Box>
                     <Box>
-                        <Image width="0%" height="50%" layout="fill" objectFit="cover" src={slider3} />
+                        <Image width="100%" height="40%" layout="responsive" objectFit="cover" src={slider2} />
                     </Box>
 
 
